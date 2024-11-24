120aafcfc1ada87e58b851e0ff723580ac7e27a8
abait-ou <abait-ou@DESKTOP-6V2PUQV>
2024-11-24 18:31:25 +0100

    Sign Up

--- Signup.php
+++ Signup.php
@@ -6,7 +6,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="./Style.css" rel="Stylesheet"> 
-    <title>Carrental - Sign In</title>
+    <title>Carrental - Sign Up</title>
 </head>
 <body>
 
@@ -15,7 +15,7 @@
     <h1>Carrental</h1>
     <div class="buttons">
         <button>Home <img src="./Assets/Sign_in/Home_fill.png" alt=""></button>
-        <button>Sign In <img src="./Assets/Sign_in/User_fill_add.png" alt=""></button>
+        <button>Sign Up <img src="./Assets/Sign_in/Sign_in_squre_duotone.png" alt=""></button>
     </div>
 </section>
 <!-- Navbar Section End  -->
@@ -30,17 +30,20 @@
         </div>
         <div class="titlesub">
             <h3>Sign In</h3>
-            <p>Open Your Account And Choose Your<br>
-            Favourite Car And The Rest Is On US </p>
+            <p>Create Your Own Account And Start <br>
+            Discovering Our Flotte </p>
         </div>
         <div class="inputs">
+            <input type="text" placeholder="First Name">
+            <input type="text" placeholder="Last Name">
             <input type="email" placeholder="Email Adress">
             <input type="password" placeholder="Password">
+            <input type="text" placeholder="Phone Number">
         </div>
         <hr>
         <div class="buttons">
-            <button>Forget Your Password ?</button>
-            <button>Sign In <img src="./Assets/Sign_in/Sign_in_squre_duotone.png" alt=""></button>
+            <button>Already Have An Account ?</button>
+            <button>Sign Up <img src="./Assets/Sign_up/User_fill_add.png" alt=""></button>
         </div>
     </form>
 </section>
